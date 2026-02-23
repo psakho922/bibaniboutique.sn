6850deb0b9fd9a9d5f39f92ca206b862db23df74
psakho922 <psakho922@gmail.com>
2026-02-23 03:09:58 +0000

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,15 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
 
 async function bootstrap() {
-  const server = express();
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(server),
-  );
+  const app = await NestFactory.create(AppModule);
 
+  // IMPORTANT pour Render
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
